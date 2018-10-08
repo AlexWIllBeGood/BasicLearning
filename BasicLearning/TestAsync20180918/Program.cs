cf34fe51e7912a6d3524f1fcf0cae78b4c98e599
Alex <AlexWillBeGood@163.com>
2018-10-08 17:43:16 +0800

    commom commit

---
+++ BasicLearning/TestAsync20180918/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAsync20180918
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class WebContentAsync
+    {
+        provaye 
+        Stopwatch sw = null;
+        public WebContentAsync()
+        {
+            sw = new Stopwatch();
+        }
+        public 
+    }
+}
---
+++ BasicLearning/TestWebApi20181008/Models/ContactRepository.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApi20181008.Models
+{
+    public class ContactRepository : IContactRepository
+    {
+        IMongoClient client = null;
+        IMongoDatabase database = null;
+        IMongoCollection<Contact> contacts = null;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public ContactRepository(string connection)
+        {
+            if (!string.IsNullOrWhiteSpace(connection))
+            {
+                client = new MongoClient(connection);
+            }
+            else
+            {
+                client = new MongoClient();
+            }
+            database = client.GetDatabase("TestDb");
+            contacts = database.GetCollection<Contact>("foo");
+        }
+        public Contact AddContact(Contact item)
+        {
+            item.Id = ObjectId.GenerateNewId().ToString();
+            item.LastModified = DateTime.Now;
+            contacts.InsertOne(item);
+            return item;
+        }
+
+        public IEnumerable GetAllContacts()
+        {
+            return contacts.Find(null).ToList();
+        }
+
+        public Contact GetContact(string id)
+        {
+            return contacts.Find(e => e.Id == id).FirstOrDefault();
+;        }
+
+        public bool RemoveContact(string id)
+        {
+            long deletedcount=contacts.DeleteOne(e=>e.Id==id).DeletedCount;
+            return deletedcount == 1;
+        }
+
+        public bool UpdateContact(string id, Contact item)
+        {
+            var updateBuilder = Builders<Contact>.Update.Set("Name", item.Name).Set("Phone", item.Phone).Set("LastModified", item.LastModified);
+            long modifiedCount=contacts.UpdateOne(e => e.Id == id, updateBuilder).ModifiedCount;
+            return modifiedCount == 1;
+        }
+    }
+}
