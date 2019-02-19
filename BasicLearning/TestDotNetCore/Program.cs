fe0d236de46181f4169f4633d3fa12741fb94ff9
Alex <AlexWillBeGood@163.com>
2019-02-19 17:36:09 +0800

    common commit

---
+++ BasicLearning/TestDotNetCore/Program.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Windows.Input;
+
+namespace TestDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection connection;
+
+            connection = new NpgsqlConnection("Server=168.33.162.63;Database=FYUNDB;user id=sa;password=fanyun@2018;");
+            Console.WriteLine(GetServerVersion(connection));
+            Console.ReadKey();
+        }
+        public static string GetServerVersion(IDbConnection connection)
+        {
+            ConnectionState originalState = connection.State;
+            if (originalState != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            try
+            {
+                IDbCommand command = connection.CreateCommand();
+                command.CommandText = "select count(*) from dbo.fy_employee";
+                return command.ExecuteScalar().ToString();
+            }
+            finally
+            {
+                if (originalState == ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
